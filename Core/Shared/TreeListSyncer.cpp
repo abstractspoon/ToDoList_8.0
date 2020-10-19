aca14009c15d24b3c923c47e534bf488d31c6afc
abstractspoon <daniel.godson@gmail.com>
2020-10-19 20:46:26 +1100

    'Fix' smooth mouse-wheel scroll reversal

--- Core/Shared/TreeListSyncer.cpp
+++ Core/Shared/TreeListSyncer.cpp
@@ -2707,6 +2707,16 @@ BOOL CTreeListSyncer::HandleMouseWheel(HWND hWnd, WPARAM wp, LPARAM lp)
 
 	if (hwndTree)
 	{
+		// There's a very strange bug (probably caused somehow 
+		// by the following method) where during a long, smooth
+		// scroll, the scroll direction suddenly reverses.
+		// My investigations thus far reveal that when the 
+		// direction changes the zDelta value is NOT a multiple
+		// of 120, so for now I'm going to try ignoring such
+		// values and hope it doesn't break anything else
+		if ((zDelta % 120) != 0)
+			return TRUE; // we handled it
+		
 		while (nNumClicks--)
 		{
 			HTREEITEM htiPrevFirstVis = TreeView_GetFirstVisible(hwndTree), htiFirstVis = htiPrevFirstVis;
